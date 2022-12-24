cb5a46488103614a01634d41fed030cdd6eaec13
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2022-12-23 22:21:49 -0300

    feat: add constants file

---
+++ constants.py
@@ -0,0 +1,32 @@
+# csv data file path
+CSV_FILEPATH = "dataset/worldcities.csv"
+
+# the countries to get cities data from
+COUNTRIES = [
+  "Argentina",
+  "Bolivia",
+  "Brazil",
+  "Chile",
+  "Colombia",
+  "Ecuador",
+  "Paraguay",
+  "Peru",
+  "Uruguay",
+  "Venezuela"
+]
+
+# a connection is added to a nodes that has a distance lower than this value, between them
+MAX_DISTANCE = 200
+
+# html file name for the network visualization
+HTML_FILENAME = "networks/main.html"
+
+# the countries to get the capital from
+COUNTRIES_TO_GET_CAPITAL_FROM = [
+  "Argentina",
+  "Brazil",
+  "Chile",
+]
+
+# tests iterations
+QUANTITY_OF_TESTS_ITERATIONS = 10
